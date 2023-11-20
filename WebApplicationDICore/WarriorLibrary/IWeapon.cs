cc52bc49dbff86d1b07a1ad3a573e7a9a2ba4b55
Meyers, Jeff <jmeyers@colum.edu>
2023-11-20 16:28:33 -0600

    Web DI

---
+++ WebApplicationDICore/WarriorLibrary/IWeapon.cs
@@ -0,0 +1,8 @@
+﻿namespace DIDemo
+{
+    public interface IWeapon
+    {
+        string Name { get; set;  }
+        string Hit(string target);
+    }
+}
---
+++ WebApplicationDICore/WarriorLibrary/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIDemo
+{
+    public class Weapon : IWeapon
+    {
+        protected string name;
+        public string Name { get => name; set => name = value; }
+
+        public Weapon()
+        {
+            name = "Weapon";
+        }
+
+        public string Hit(string target)
+        {
+            return $"{this.Name} on {target}";
+        }
+    }
+}
